5d6860858864cf1b97bf54a5acaad5a1a431d920
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2026-02-12 23:09:30 +0700

    method dart

---
+++ bin/method.dart
@@ -0,0 +1,14 @@
+void main() {
+  Car car = Car();
+  car.road();
+}
+
+class Car {
+  String brand = "Unknow";
+  final int wheel = 4;
+  String? color;
+
+  void road() {
+    print("Ngengg.... ngeng....");
+  }
+}
